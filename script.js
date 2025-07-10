e96c1d3b9bb9aeb45090b38a75f80fd03fbcb830
sakshamwithweb <websaksham39@gmail.com>
2025-07-10 07:23:22 +0530

    added tooltip

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ const main = async () => {
     const oneBoxValue = 25;
     const progress = document.getElementById("progress")
 
-    const data = await fetchData()
+    const data = (await fetchData()).flatMap((item) => {
+        if (item.amount > oneBoxValue) {
+            const correctItem = { ...item, amount: 25 }
+            const noOfItem = item.amount / oneBoxValue
+            return Array.from({ length: noOfItem }, () => ({ ...correctItem }))
+        } else {
+            return item
+        }
+    })
 
     const totalAmount = data.reduce((sum, item) => sum + item.amount, 0);
     const filledBoxes = Math.floor(totalAmount / oneBoxValue);
@@ -26,11 +34,15 @@ const main = async () => {
     for (let i = 0; i <= filledBoxes; i++) {
         const box = document.getElementsByClassName(`box`)[i]
         if (i < filledBoxes) {
+            const tooltipText = document.createElement("span")
+            tooltipText.classList.add("tooltiptext")
+            tooltipText.innerText = `${data[i].title}: ${data[i].description}`
+            box.appendChild(tooltipText)
             box.setAttribute("id", "filled")
+            box.classList.add("tooltip")
         } else {
             box.setAttribute("class", "near")
         }
-        console.log(box)
     }
 }
 
